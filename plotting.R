88fe0021d6d833a520eda2760d9cd6081a9a0518
Robinson <robi5767@vandals.uidaho.edu>
2018-11-29 16:32:38 -0800

    Added plotting functions from the "rethinking" package

--- plotting.R
+++ plotting.R
@@ -1,3 +1,48 @@
+#Plotting functions from Richard McElreath's "rethinking" package
+col.alpha <- function( acol , alpha=0.2 ) {
+  acol <- col2rgb(acol)
+  acol <- rgb(acol[1]/255,acol[2]/255,acol[3]/255,alpha)
+  acol
+}
+shade <- function( object , lim , label=NULL , col=col.alpha("black",0.15) , border=NA , ... ) {
+  if ( missing(lim) ) stop( "Interval limits missing." )
+  if ( missing(object) ) stop( "No density or formula object." )
+  from <- lim[1]
+  to <- lim[2]
+  if ( class(object)=="formula" ) {
+    # formula input
+    x1 <- eval( object[[3]] )
+    y1 <- eval( object[[2]] )
+    x <- x1[ x1>=from & x1<=to ]
+    y <- y1[ x1>=from & x1<=to ]
+  }
+  if ( class(object)=="density" ) {
+    # density input
+    x <- object$x[ object$x>=from & object$x<=to ]
+    y <- object$y[ object$x>=from & object$x<=to ]
+  }
+  if ( class(object)=="matrix" & length(dim(object))==2 ) {
+    # matrix defining confidence region around a curve
+    y <- c( object[1,] , object[2,][ncol(object):1] ) # reverse second row
+    x <- c( lim , lim[length(lim):1] ) # lim needs to be x-axis values
+  }
+  # draw
+  if ( class(object)=="matrix" ) {
+    polygon( x , y , col=col , border=border , ... )
+  } else {
+    polygon( c( x , to , from ) , c( y , 0 , 0 ) , col=col , border=border , ... )
+  }
+  # label?
+  if ( !is.null(label) ) {
+    lx <- mean(x)
+    ly <- max(y)/2
+    text( lx , ly , label )
+  }
+}
+
+#function to collect quantiles
+QUANT <- function(x) quantile(x,probs = c(0.1,0.25,0.5,0.75,0.9))
+
 PLOT_DOY <- function(n,s){
   hist_vector <- rep(0,55)
   hist_matrix <- matrix(NA,20,55)
