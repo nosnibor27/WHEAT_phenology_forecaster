4142a1e66b386e44cce9bdc4aeacd11accc4b8ad
Robinson <robi5767@vandals.uidaho.edu>
2018-11-29 16:59:09 -0800

    Removed URL sequences to netcdf_downloader.r

--- init.R
+++ init.R
@@ -52,41 +52,6 @@ for (n in 1:N){
   locations$elevation[n] <- 0.1*elevation_ref[lonindex,latindex]
 }
 
-#base URL
-url_1 <- "http://thredds.northwestknowledge.net:8080/thredds/dodsC/NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/macav2livneh/"
-
-#model list
-model <- c("bcc-csm1-1", "bcc-csm1-1-m","BNU-ESM","CanESM2",
-           "CSIRO-Mk3-6-0","GFDL-ESM2G","GFDL-ESM2M","HadGEM2-CC365",
-           "HadGEM2-ES365","inmcm4","IPSL-CM5A-LR","IPSL-CM5A-MR",
-           "IPSL-CM5B-LR","MIROC5","MIROC-ESM","MIROC-ESM-CHEM",
-           "MRI-CGCM3","NorESM1-M","CNRM-CM5","CCSM4")
-
-#model condition list becuase CCSM4 is different
-condition <- c(rep("_r1i1p1_",19),"_r6i1p1_")
-
-#timestep lists
-timestep_historical <- c("historical_1950_1969_CONUS_daily.nc",
-                         "historical_1970_1989_CONUS_daily.nc",
-                         "historical_1990_2005_CONUS_daily.nc")
-timestep_45 <- c("rcp45_2006_2025_CONUS_daily.nc",
-                 "rcp45_2026_2045_CONUS_daily.nc",
-                 "rcp45_2046_2065_CONUS_daily.nc",
-                 "rcp45_2066_2085_CONUS_daily.nc",
-                 "rcp45_2086_2099_CONUS_daily.nc")
-timestep_85 <- c("rcp85_2006_2025_CONUS_daily.nc",
-                 "rcp85_2026_2045_CONUS_daily.nc",
-                 "rcp85_2046_2065_CONUS_daily.nc",
-                 "rcp85_2066_2085_CONUS_daily.nc",
-                 "rcp85_2086_2099_CONUS_daily.nc")
-
-#variable list
-variable_1 <- c("_tasmax_","_tasmin_","_pr_","_huss_","_was_","_rsds_")
-
-#reference longitude and latitude sequence
-lon <- seq(235.40625,292.96875,0.0625)
-lat <- seq(25.15625,52.84375,0.0625)
-
 #downloading climate data
 source("netcdf_downloader.r")
 
